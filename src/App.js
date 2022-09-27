d26fc2922b21bd206065644d97cd18d9004e53ab
Allen Ramos <allencarlanz.ramos@outlook.com>
2022-09-27 11:06:43 +0800

    Initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import Login from './components/Login';
 
-function App() {
+let user = {
+  name: "Allen",
+  position: "Instructor",
+  avatar: "https://i.postimg.cc/63wdNgF7/user-sign-icon-person-symbol-human-avatar-vector-12693195.jpg"
+}
+
+const products = [
+  { name: 'Soap', price: 30 },
+  { name: 'Shampoo', price: 100 },
+  { name: 'Garlic', price: 80 }
+];
+
+function UserDetails({ props }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img src={user.avatar} alt="avatar" />
+      <p>Position: {user.position}</p>
+      <p>Hiring Date: {props.hiringDate}</p>
+      <p>Gender: {props.gender}</p>
+      <hr />
+    </>
   );
 }
+
+function List() {
+  return (
+    <>
+      <h1 className="list-head">{user.name}'s Grocery List</h1>
+      <ul>
+        <ListItem props={{ name: "Soap", price: 20 }} />
+        <ListItem props={{ name: "Shampoo", price: 100 }} />
+        <ListItem props={{ name: "Egg", price: 80 }} />
+      </ul>
+    </>
+  );
+}
+
+function ListItem({ props }) {
+  return <li>{props.name} - {props.price}</li>;
+}
+
+function App() {
+  let info = {
+    hiringDate: "August 2022",
+    gender: "Male"
+  };
+
+  let isLoggedIn = true;
+  let content;
+
+  if (isLoggedIn) {
+    content =
+      <>
+        <UserDetails props={info} />
+        <List />
+      </>;
+  }
+  else {
+    content = <Login />;
+  }
+
+  return content;
+
+}
+
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Login() {
+    return (
+        <>
+            <h1>Please login your account</h1>
+            <input type="text" />
+            <button>Login</button>
+        </>
+    );
+}
+
+export default Login;
