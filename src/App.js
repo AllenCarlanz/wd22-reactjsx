9ceac42d2b21c842244de391c600dc8b0d6b7c80
Allen Ramos <allencarlanz.ramos@outlook.com>
2022-10-07 12:00:14 +0800

    Final demo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,66 @@
-import './css/App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import StudentList from './components/StudentList/StudentList';
-
 function App() {
+  const products = [
+    {
+      id: 1,
+      name: "Mouse",
+      color: "Black",
+      price: 6000,
+      qty: 3
+    },
+    {
+      id: 2,
+      name: "Keyboard",
+      color: "White",
+      price: 3000,
+      qty: 5
+    },
+    {
+      id: 3,
+      name: "Speakers",
+      color: "Black",
+      price: 10000,
+      qty: 2
+    }
+  ];
+
+  function updateProduct(productId, newQty) {
+
+    let updatedList = products.map(product => {
+
+      let quantity = product.qty;
+
+      if (productId == product.id) {
+        quantity = quantity + newQty;
+      }
+
+      return (
+        {
+          id: product.id,
+          name: product.name,
+          color: product.color,
+          price: product.price,
+          qty: quantity
+        }
+      );
+    });
+
+    console.log(updatedList);
+
+  }
+
+  updateProduct(2, 3);
+
+  // function removeItem() {
+  //   let updatedList = products.filter(product => product.price < 5000);
+
+  //   console.log(updatedList);
+  // }
+
   return (
     <>
-      <Header />
-      <StudentList />
-      <Footer />
+      <h1>React App</h1>
     </>
   );
 }
-
 
 export default App;
--- src/components/StudentList/StudentListItem.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function StudentListItem({ name }) {
-    return <li key={name}>{name}</li>;
-}
-
-export default StudentListItem;
